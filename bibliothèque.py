0ba03c5ee615143037c9c96334e291263f26d1ae
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2025-11-04 21:22:21 +0100

    vérifier si la DB existe avant de l'ouvrir

--- bibliothèque.py
+++ bibliothèque.py
@@ -1,14 +1,24 @@
 #!/bin/python3
 # ^^^ pratique pour pouvoir lancer sur linux './bibliothèque.py' (faut juste pas oublier le chmod +x)
 
+import os
 import sqlite3 as sgbd
 import random as r
 # import qrcode
 import lib.menu as menu # local, voir ./lib/menu.py
 from lib.fonctions import * # local, voir ./lib/fonctions.py
 
-
 ### debut du programme principal
+
+if(not(os.path.exists("biblio.db"))):
+    print("""
+\033[1;31m!!! La base de donnée n'existe pas !!!\033[0m
+
+\033[1mPour créer la base de données, executez la commande:\033[0m
+
+    python3 scripts/make_db.py
+""")
+    exit(1)
 # connecter sqlite a la DB et connecter un curseur
 connexion = sgbd.connect("biblio.db")
 curseur = connexion.cursor()
