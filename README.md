db1450911151f018161cb51718754b27cd29c3c5
heliumhydride <34401203+heliumhydride@users.noreply.github.com>
2025-11-04 11:37:55 +0100

    script pour créer la DB + readme

--- README.md
+++ README.md
@@ -8,8 +8,52 @@ un logiciel de gestion de bibliothèque en [TUI](https://fr.wikipedia.org/wiki/E
 C'est pour un projet de NSI en Terminale sur les bases de données.
 
 ### 🚀 installation
-Clonez ce depôt, puis ouvrez une cmd/un terminal dedans et lancez:
+Clonez ce depôt, puis ouvrez une cmd/un terminal.
+Commencez par initialiser une base de données:
+```sh
+python3 scripts/make_db.py
+```
+
+Le processus est guidé:
+```
+La DB n'existe pas.
+
+quelle base de donnée voulez vous initialiser?
+1. Base de donnée vide pour la production (recommandé) >>>
+2. Base de donnée avec des exemples (pour tester le logiciel) >>>
+
+* si vous ne savez pas, tapez '1'
+
+--> ...
+```
+
 ```sh
 python3 bibliothèque.py
 ```
 * !!! Si vous êtes sur Windows, n'utilisez pas 'py', mais bien 'python' ou 'python3'. 'py' n'aime pas le 'è' dans le nom du fichier, je ne sais pas pourquoi. !!!
+
+### 💻 developpement
+Les fonctionnalités sont a ajouter dans `lib/fonctions.py`
+Les menus / tout en rapport avec l'interface est dans `lib/menu.py`
+
+Il y a un script pour créer des menus très facilement (`scripts/creer_menu.py`)
+
+Utilisation:
+```
+scripts/creer_menu.py [elt1] [elt2] ...
+```
+
+Exemple:
+```
+scripts/creer_menu.py "Truc 1 ->" "Truc 2 ->" "Fermer (x)"
+['Truc 1 ->', 'Truc 2 ->', 'Fermer (x)']
++---------------------+
+|                     |
+|    1. Truc 1 ->     |
+|    2. Truc 2 ->     |
+|    3. Fermer (x)    |
+|                     |
++---------------------+
+```
+
+Il suffit de faire copier coller du terminal dans un print("""...""")
---
+++ scripts/make_db.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+import os
+import sqlite3 as sgbd
+
+def print_status(type:str, s:str):
+    couleur = 0
+    if(type == "warning"):
+        couleur = 33
+    elif(type == "error"):
+        couleur = 31
+    elif(type == "success"):
+        couleur = 32
+    elif(type == "note"):
+        couleur = 34
+    print(f"\033[1;{couleur}m{s}\033[0m")
+
+# On vérifie qu'on est bien dans le dossier racine et pas dans scripts/
+if(os.path.exists("make_db.sql") and not(os.path.exists("bibliothèque.py"))):
+    print_status("error", "Il faut être dans le dossier racine, pas dans le dossier scripts.")
+    exit(1)
+
+# Si la DB existe déjà on met une alerte
+if(os.path.exists("biblio.db")):
+    print_status("warning", "La base de donnée existe déjà! Si vous continuez, vous écraserez l'ancienne base de donnée bibliothèque. Continuer?")
+    choix_ecraser_db = input("[y/n]? --> ")
+    if(choix_ecraser_db not in ['y','Y']):
+        print_status("success", "=> D'accord, on annule tout")
+        exit(1)
+else:
+    print_status("note", "La DB n'existe pas.")
+
+print("""
+quelle base de donnée voulez vous initialiser?
+1. Base de donnée vide pour la production (recommandé) >>>
+2. Base de donnée avec des exemples (pour tester le logiciel) >>>
+
+* si vous ne savez pas, tapez '1'
+""")
+
+choix_type_db = int(input("--> "))
+if(choix_type_db not in [1,2]):
+    print_status("error", "1 ou 2 !!!")
+    exit(1)
+
+# On ouvre créé la DB
+connex = sgbd.connect("biblio.db")
+c = connex.cursor()
+
+# volé gracieusement de https://stackoverflow.com/questions/19472922/reading-external-sql-script-in-python
+if(choix_type_db == 1):
+    fd = open('scripts/script_make_db_mini.sql')
+elif(choix_type_db == 2):
+    fd = open('scripts/script_make_db_exemples.sql')
+
+script_sql = fd.read()
+fd.close()
+
+commandes_sql = script_sql.split(';')
+
+for cmd in commandes_sql:
+    try:
+        c.execute(cmd)
+    except sgbd.OperationalError as msg:
+        print_status("error", f"""
+!!! La commande suivante !!! {cmd}
+!!! A échouée avec l'erreur !!!
+
+{msg}
+""")
+
+connex.commit()
+connex.close()
